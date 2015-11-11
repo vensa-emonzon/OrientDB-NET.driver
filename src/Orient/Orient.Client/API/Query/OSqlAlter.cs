7dddafcb6b429fa26aeca6ee79bf306d9ce39b9b
Eduardo Monzon <emonzon@vensahealth.com>
2015-11-11 16:16:43 +1300

    Adding missing files from our existing code base

---
+++ src/Orient/Orient.Client/API/Query/OSqlAlter.cs
@@ -0,0 +1,80 @@
+﻿using Orient.Client.API.Query;
+using Orient.Client.Protocol;
+using Orient.Client.Protocol.Operations.Command;
+
+// ReSharper disable UnusedMember.Global
+
+// syntax: 
+// DROP CLASS <class> 
+
+namespace Orient.Client
+{
+    public class OSqlAlter
+    {
+        private readonly Connection _connection;
+        private OCommandQuery _command;
+
+        #region Constructors
+
+        internal OSqlAlter(Connection connection)
+        {
+            _connection = connection;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public OSqlAlter Abstract<T>(bool makeAbstract)
+        {
+            var className = typeof(T).Name;
+            var payload = new CommandPayloadCommand { Text = $"ALTER CLASS {className} ABSTRACT {makeAbstract}" };
+            _command = new OCommandQuery(_connection, payload);
+            return this;
+        }
+
+        public OSqlAlter Superclass(string className, string superclassName)
+        {
+            var payload = new CommandPayloadCommand { Text = $"ALTER CLASS {className} SUPERCLASS {superclassName}" };
+            _command = new OCommandQuery(_connection, payload);
+            return this;
+        }
+
+        public OSqlAlter Superclass<T, TU>()
+        {
+            return Superclass(typeof(T).Name, typeof(TU).Name);
+        }
+
+        public OSqlAlter AddCluster<T>(string clusterName)
+        {
+            var className = typeof(T).Name;
+            var payload = new CommandPayloadCommand { Text = $"ALTER CLASS {className} ADDCLUSTER {clusterName}" };
+            _command = new OCommandQuery(_connection, payload);
+            return this;
+        }
+
+        public OSqlAlter RemoveCluster<T>(int clusterId)
+        {
+            var className = typeof(T).Name;
+            var payload = new CommandPayloadCommand { Text = $"ALTER CLASS {className} REMOVECLUSTER {clusterId}" };
+            _command = new OCommandQuery(_connection, payload);
+            return this;
+        }
+
+        public int Run()
+        {
+            return _command.Run().GetScalarResult();
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override string ToString()
+        {
+            return _command.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Orient/Orient.Client/API/Query/OSqlDeleteClass.cs
@@ -0,0 +1,53 @@
+﻿using Orient.Client.API.Query;
+using Orient.Client.Protocol;
+using Orient.Client.Protocol.Operations.Command;
+
+// ReSharper disable UnusedMember.Global
+
+// syntax: 
+// DROP CLASS <class> 
+
+namespace Orient.Client
+{
+    public class OSqlDeleteClass
+    {
+        private readonly Connection _connection;
+        private OCommandQuery _command;
+
+        #region Constructors
+
+        internal OSqlDeleteClass(Connection connection)
+        {
+            _connection = connection;
+        }
+
+        #endregion
+
+        #region Class
+
+        public OSqlDeleteClass Drop(string className)
+        {
+            var payload = new CommandPayloadCommand { Text = $"DROP CLASS {className}" };
+            _command = new OCommandQuery(_connection, payload);
+            return this;
+        }
+
+        public OSqlDeleteClass Drop<T>()
+        {
+            return Drop(typeof(T).Name);
+        }
+
+        #endregion
+
+        public int Run()
+        {
+            return _command.Run().GetScalarResult();
+        }
+
+
+        public override string ToString()
+        {
+            return _command.ToString();
+        }
+    }
+}
